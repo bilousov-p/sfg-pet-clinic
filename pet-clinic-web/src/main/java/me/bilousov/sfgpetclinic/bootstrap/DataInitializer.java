22ba8c3105739ddfd92383ab002b2c250e45ad80
bilousov-p <bilousov.p@icloud.com>
2022-08-18 23:08:13 +0200

    add PetType to bootstrap. closes #32

--- pet-clinic-web/src/main/java/me/bilousov/sfgpetclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/me/bilousov/sfgpetclinic/bootstrap/DataInitializer.java
@@ -1,8 +1,10 @@
 package me.bilousov.sfgpetclinic.bootstrap;
 
 import me.bilousov.sfgpetclinic.model.Owner;
+import me.bilousov.sfgpetclinic.model.PetType;
 import me.bilousov.sfgpetclinic.model.Vet;
 import me.bilousov.sfgpetclinic.service.OwnerService;
+import me.bilousov.sfgpetclinic.service.PetTypeService;
 import me.bilousov.sfgpetclinic.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,26 @@ public class DataInitializer implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
 
-    public DataInitializer(OwnerService ownerService, VetService vetService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Micheal");
         owner1.setLastName("Weston");

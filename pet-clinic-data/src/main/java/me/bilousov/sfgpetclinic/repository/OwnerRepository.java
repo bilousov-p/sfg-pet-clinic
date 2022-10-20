94ee40d855a2218b323674dcb2724d346621432a
bilousov-p <bilousov.p@icloud.com>
2022-10-20 23:08:43 +0200

    add Spring Data JPA service for Owner. Closes #40

--- pet-clinic-data/src/main/java/me/bilousov/sfgpetclinic/repository/OwnerRepository.java
+++ pet-clinic-data/src/main/java/me/bilousov/sfgpetclinic/repository/OwnerRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
